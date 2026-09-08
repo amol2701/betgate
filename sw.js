b13509b202c00570d4237ccbee8434bdd81b02a7
amol2701 <50322806+amol2701@users.noreply.github.com>
2026-09-08 10:43:34 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "betgate-v2";
+const CACHE = "betgate-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
